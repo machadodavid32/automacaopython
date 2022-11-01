67d73e76adc3dac3840aed4ba386d904a5d6af08
David Machado <machadodavid32@gmail.com>
2022-11-01 16:38:06 -0300

    Adicionando arquivo click

---
+++ selenium_m2/click_selenium.py
@@ -0,0 +1,34 @@
+from fileinput import close
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+botao_dropdown = driver.find_element(By.ID, 'dropdownMenuButton')
+#botao_dropdown.click()  # Primeira forma de clickar
+driver.execute_script('arguments[0].click()', botao_dropdown)  # segunda forma de clicar, neste caso, é executado um comando javascript dentro do python
+
+input('')
+driver.close()
