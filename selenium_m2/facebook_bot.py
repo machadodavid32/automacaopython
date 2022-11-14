294098d6d131e475adae0a21088a1d926d8d4ce8
David Machado <machadodavid32@gmail.com>
2022-11-14 16:23:31 -0300

    Terminando bot para facebook

--- selenium_m2/facebook_bot.py
+++ selenium_m2/facebook_bot.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select  # Serve para trabalhar com dropdown
 import os, random
-from digitar_com_selenium import digitar_naturalmente
+
 
 
 def iniciar_driver():
@@ -30,7 +30,7 @@ def iniciar_driver():
 
 # Passos para automação Facebook
 
-# Clicar dentro do campo de status
+
 # Digitar algo
 # Clicar em publicar
 
@@ -42,12 +42,12 @@ sleep(2)
 # Digitar email
 email = driver.find_element(By.ID, 'email')
 sleep(3)
-email.send_keys('davidinrock@hotmail.com')
+email.send_keys('**********')
 
 # Digitar senha
 senha = driver.find_element(By.ID, 'pass')
 sleep(2)
-senha.send_keys('davidaline')
+senha.send_keys('**********')
 
 # Clicar em login
 entrar = driver.find_element(By.XPATH, "//button[@name='login']")
@@ -58,17 +58,32 @@ publicacao = driver.find_element(By.XPATH, "//div[@class='xi81zsa x1lkfr7t xkjl1
 publicacao.click()
 sleep(1)
 
+# Clicar dentro do campo de "somente eu"
 somente_eu = driver.find_element(By.XPATH, "//*[text()='Somente eu']")
 somente_eu.click()
 sleep(2)
 
+# Clicar confirmar a escolha
 concluir = driver.find_element(By.XPATH, '//*[text()="Concluir"]')
 sleep(1)
 concluir.click()
 
+def digitacao_humana(texto, variavel):
+    for letra in texto:
+        variavel.send_keys(letra)
+        sleep(random.randint(1,5)/30)
+
+# Acima fiz um função para escrever de forma humana.
 escrever = driver.find_element(By.XPATH, '//p[@class="x16tdsg8 x1mh8g0r xat24cr x11i5rnm xdj266r"]')
 sleep(3)
-digitar_naturalmente('oi, é só um teste', escrever)
+digitacao_humana('oi, é somente um teste', escrever)
+sleep(2)
+
+# Clicar em publicar
+publicar = driver.find_element(By.XPATH, "//*[text()='Publicar']")
+sleep(2)
+publicar.click()
+
 
 input('')
 driver.close()
